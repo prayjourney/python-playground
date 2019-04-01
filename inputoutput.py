595206e09eae4a913b9a83108234fbb7d4f31392
zuiguangyin <2247359268@qq.com>
2019-04-01 03:50:43 +0000

    添加输入输出的操作,持续输入

---
+++ inputoutput.py
@@ -0,0 +1,34 @@
+# coding =utf8
+
+def inputwords(path):
+    '''
+    持续输入文字
+    '''
+    with open(file = path, mode = "a+", encoding = "utf8") as f:
+        x = input()
+        while(x !="quit"):
+            '''
+            持续输入, 直到输入quit, 才退出
+            '''
+            f.write(x + "\n")
+            x = input()
+    print("输入完毕.")
+    # python文件读写模式的总结
+    # https://www.cnblogs.com/zhxwind/p/8761618.html
+    # https://www.cnblogs.com/dadong616/p/6824859.html
+
+
+def getinputcontent(path):
+    '''
+    获取输入的内容
+    '''
+    with open(file = path, mode = "r+", encoding = "utf8") as f:
+        all_content = f.read()
+        print(all_content)
+    
+if __name__ == "__main__":
+    path = "c://Users//r00478401//Desktop//le//xxx.txt"
+    print("即将开始输入工作...\n")
+    inputwords(path)
+    print("内容是:\n")
+    getinputcontent(path)
