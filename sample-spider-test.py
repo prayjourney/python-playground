23722f7e5524b163e093abeed3f800a8b44e8c46
zuiguangyin <2247359268@qq.com>
2019-04-15 14:07:35 +0000

    文本格式化输出

--- sample-spider-test.py
+++ sample-spider-test.py
@@ -74,3 +74,8 @@ __init__.py的主要作用是:
 2. 定义__all__用来模糊导入
 3. 编写Python代码(不建议在__init__中写python模块, 可以在包中在创建另外的模块来写, 尽量保证__init__.py简单)
 '''
+
+# 格式化文本或者字符串
+# %s是字符串, %d是数字
+s123 = "nihoa %s, dsfafdsafasf %s , %d"%("123dsfasf","ffff",10)
+print(s123)
