02a4d0112e9410a5b2ac086bb5ab9381d8d75e4c
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-09-29 19:53:48 +0800

    🚀🚀🚀点击事件的响应

--- pyside2ui/first_pyside2_demo.py
+++ pyside2ui/first_pyside2_demo.py
@@ -2,6 +2,11 @@ from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QPlainText
 
 app = QApplication([])
 
+# 定义一个槽函数，用于事件处理
+def handleClick():
+    print(123)
+
+
 window = QMainWindow()
 window.resize(500, 400)
 window.move(300, 310)
@@ -14,6 +19,8 @@ textEdit.resize(300, 350)
 
 button = QPushButton('统计', window)
 button.move(380, 80)
+# 点击事件的响应
+button.clicked.connect(handleClick)
 
 window.show()
 
