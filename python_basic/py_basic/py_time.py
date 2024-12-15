62052a2b8df2d55418356b0c962d8271ac7f57bb
prayjourney <prayjourney@outlook.com>
2024-12-15 11:25:08 +0800

    🎏time的时间操作 2

--- python_basic/py_basic/py_time.py
+++ python_basic/py_basic/py_time.py
@@ -5,8 +5,18 @@ import pygame
 
 
 def uset_time():
-    time1 = time.time()
+    time1 = time.time()   # 获取当前的时间戳
     print(time1)
+    local_time_tuple = time.localtime(time1)
+    print(local_time_tuple) # 获取结构性的时间
+    print(local_time_tuple.tm_year)
+    print(time.gmtime())     # 获取结构性的时间
+    time.sleep(1) # 休眠1秒
+    print("time is: ", time.strftime("%Y-%m-%d %H:%M:%S", local_time_tuple)) # 格式化时间
+    print("time is: ", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+
+
+    # print(time1.strftime("%Y %m %d %H:%M:%S", time1))
 
 
 def set_alarm(alarm_time):
@@ -38,5 +48,5 @@ def set_alarm(alarm_time):
 
 if __name__ == '__main__':
     uset_time()
-    alarm_time = input("enter time (HH:MM:SS)")
-    set_alarm(alarm_time)
+    # alarm_time = input("enter time (HH:MM:SS)")
+    # set_alarm(alarm_time)
