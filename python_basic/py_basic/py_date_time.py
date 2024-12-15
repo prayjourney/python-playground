3c2c5824e556029ee9df4c84ab1b5d2bb09690a2
prayjourney <prayjourney@outlook.com>
2024-12-15 11:10:47 +0800

    🎏date_time的时间操作

--- python_basic/py_basic/py_date_time.py
+++ python_basic/py_basic/py_date_time.py
@@ -1,4 +1,23 @@
 import  datetime
 
-def time_get():
-    datetime.datetime.day.var
+def get_date_time():
+
+
+    date = datetime.date(2022, 1,12) # 设置时间
+    print(date)
+    today = datetime.date.today()
+    print(today)
+    time = datetime.time(12, 20, 6)  # 设置时间
+    print(time)
+    now = datetime.datetime.now()
+    print(now)
+
+    now_format = now.strftime("%H:%M:%S %m-%d-%y")
+    print(now_format)
+
+    date_time = datetime.datetime(2008, 12, 28, 1,10, 33)  # 设置时间
+    print(date_time)
+
+
+if __name__ == '__main__':
+    get_date_time()
