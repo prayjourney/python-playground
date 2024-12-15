00e35da0174b240896eda0cef2f0afeba9b9aa7e
prayjourney <prayjourney@outlook.com>
2024-12-15 11:44:04 +0800

    🙂datetime的运算和格式化

--- python_basic/py_basic/py_date_time.py
+++ python_basic/py_basic/py_date_time.py
@@ -1,22 +1,30 @@
-import  datetime
+import datetime
 
 def get_date_time():
 
 
     date = datetime.date(2022, 1,12) # 设置时间
     print(date)
-    today = datetime.date.today()
+    today = datetime.date.today() # 获取当天日期
     print(today)
     time = datetime.time(12, 20, 6)  # 设置时间
     print(time)
-    now = datetime.datetime.now()
+    now = datetime.datetime.now() # 获取当前时间
     print(now)
 
-    now_format = now.strftime("%H:%M:%S %m-%d-%y")
+    now_format = now.strftime("%H:%M:%S %m-%d-%y") # 对象转换为字符串
     print(now_format)
+    date_str = "2024-01-15"
+    date_format = "%Y-%m-%d"
+    parsed_date = datetime.datetime.strptime(date_str, date_format)  # 字符串转换为对象类型
+    print("parsed date: ",parsed_date)
 
     date_time = datetime.datetime(2008, 12, 28, 1,10, 33)  # 设置时间
     print(date_time)
+
+
+    next_day = today + datetime.timedelta(days=1)    # 时间运算
+    print("next_day: ", next_day)
 
 
 if __name__ == '__main__':
